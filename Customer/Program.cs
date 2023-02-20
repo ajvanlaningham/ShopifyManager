ca8b8a1e0e9f63d9f131a8f8bf8fa37842891ca0
Alexander VanLaningham <avanlaningham@ppg.com>
2023-02-20 12:49:42 -0500

    initial commit

--- Customer/Program.cs
+++ Customer/Program.cs
@@ -26,57 +26,58 @@ namespace ShopifyCustomer
             var _productService = new ShopifyProductService(devShopifyStoreUrl, apiKey);
             var _bigListGetter = new ShopifyCustomerService(_prodCustomerService);
 
-            //List<Customer> totalCustomers = await _bigListGetter.GetAllCustomersAsync();
-            //int i = totalCustomers.Count();
-            //foreach (Customer customer in totalCustomers)
-            //{
-            //    _devCustomerService.CreateAsync(customer);
-            //    Console.WriteLine($"Created Customer {customer.FirstName} {customer.LastName}... {--i} more to go");
-            //    Thread.Sleep(100);
-            //}
-            //Console.WriteLine("Finished creating customers in dev store");
+            List<Customer> totalCustomers = await _bigListGetter.GetAllCustomersAsync();
+            int i = totalCustomers.Count();
+            foreach (Customer customer in totalCustomers)
+            {
+                _devCustomerService.CreateAsync(customer);
+                _devCustomerService.GetAsync(customer.Id);
+                Console.WriteLine($"Created Customer {customer.FirstName} {customer.LastName}... {--i} more to go");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Finished creating customers in dev store");
 
 
             // Deserialize the customer JSON into a list of Adjustments
-            var jsonPath = @"C:\Users\u526137\source\repos\Shopify\Oracle\ShopifyProduct\SharedModels\JSONSamples\Adjustment.json";
-            var json = File.ReadAllText(jsonPath);
-            RootAdjustment AdjustmentRoot = JsonConvert.DeserializeObject<RootAdjustment>(json);
+            //var jsonPath = @"C:\Users\u526137\source\repos\Shopify\Oracle\ShopifyProduct\SharedModels\JSONSamples\Adjustment.json";
+            //var json = File.ReadAllText(jsonPath);
+            //RootAdjustment AdjustmentRoot = JsonConvert.DeserializeObject<RootAdjustment>(json);
                         
-            List<Customer> customers = await _bigListGetter.GetAllCustomersAsync();
+            //List<Customer> customers = await _bigListGetter.GetAllCustomersAsync();
 
-            if (customers != null)
-            {
-                try
-                {
-                    foreach (Adjustment adjustment in AdjustmentRoot.adjustment_list.adjustment)
-                    {
-                        Customer customer = GetCorrectCustomer(customers, adjustment);
-                        if (customer != null)
-                        {
-                            Console.WriteLine($"Found customer {customer.FirstName} {customer.LastName}");
-
-
-                        }
-                        else
-                        {
-                            //TODO create a new customer in shopify and add to customer list. 
+            //if (customers != null)
+            //{
+            //    try
+            //    {
+            //        foreach (Adjustment adjustment in AdjustmentRoot.adjustment_list.adjustment)
+            //        {
+            //            Customer customer = GetCorrectCustomer(customers, adjustment);
+            //            if (customer != null)
+            //            {
+            //                Console.WriteLine($"Found customer {customer.FirstName} {customer.LastName}");
 
 
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Process error");
-                    Console.WriteLine(ex.Message);
-                }
-            }
+            //            }
+            //            else
+            //            {
+            //                //TODO create a new customer in shopify and add to customer list. 
+
+
+            //            }
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine("Process error");
+            //        Console.WriteLine(ex.Message);
+            //    }
+            //}
            
         }
 
-        private static Customer GetCorrectCustomer(List<Customer> customerList, Adjustment adjustment)
-        {
+        //private static Customer GetCorrectCustomer(List<Customer> customerList, Adjustment adjustment)
+        //{
 
-        }
+        //}
     }
 }
